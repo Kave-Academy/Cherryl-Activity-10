603b743d9b63b5b96985fedaa61efbbda1fd5309
Cherryl - Kave Blocks <cherryl@kaveblocks.com>
2022-02-24 10:55:41 +0800

    return an array of properties

--- exer.js
+++ exer.js
@@ -1,2 +1,9 @@
 function run() {
+
+
+    var student = { name : "David Rayy", 
+            sclass : "VI",
+          rollno : 12 }
+          document.getElementById("output").innerHTML = Object.getOwnPropertyNames(student);
+          console.log(Object.getOwnPropertyNames(student));
 }
